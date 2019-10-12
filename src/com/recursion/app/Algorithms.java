d9228c80c461be35ebe941dfd64e0aa7d70c1530
devjosegomez <devjosegomez@gmail.com>
2019-10-12 08:01:18 -0500

    Added sumWithFor()

--- src/com/recursion/app/Algorithms.java
+++ src/com/recursion/app/Algorithms.java
@@ -1,10 +1,23 @@
 package com.recursion.app;
 
+import java.util.Scanner;
+
 public class Algorithms {
 
 	public static void main(String[] args) {
+		int number = 0; 
+		
+		Scanner input = new Scanner(System.in);
 		Recursion recursion = new Recursion();
 		
-		System.out.println(recursion.sum(2));
+		
+		System.out.println("Enter the number to sum: ");
+		number = input.nextInt();
+		
+		System.out.println("Result with recursion: " + recursion.sum(number));
+		System.out.println("Result with for: " + recursion.sumWithFor(number));
+		
+		//close scanner
+		input.close();
 	}
 }
--- src/com/recursion/app/Recursion.java
+++ src/com/recursion/app/Recursion.java
@@ -6,4 +6,13 @@ public class Recursion {
 		if(N ==1) return 1;
 		return N + sum (N - 1);
 	}
+	
+	//Add the first N numbers using a for without recursive
+	public int sumWithFor(int N) {
+		int result = 0;
+		for(int i=1; i<=N; i++) 
+			result +=i;
+		
+		return result;
+	}
 }
